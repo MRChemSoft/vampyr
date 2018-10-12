9a40b9ae2b04920f1813979d10c8430992ee7ed1
Magnar Bjørgve <magnbjor@gmail.com>
2018-10-12 14:38:09 +0200

    Remove vamypr/project and vampyr/PyAnalytic

--- vampyr/vampyr3d.cpp
+++ vampyr/vampyr3d.cpp
@@ -15,9 +15,7 @@
 #include "trees/FunctionTree.h"
 #include "trees/FunctionTreeVector.h"
 
-#include "PyAnalyticFunction.h"
 #include "PyRepresentableFunction.h"
-#include "project.h"
 
 #include "operators/DerivativeOperator.h"
 #include "operators/ABGVOperator.h"
@@ -52,16 +50,19 @@ PYBIND11_MODULE(vampyr3d, m) {
     bases(m);
 
     m.def("project", py::overload_cast<double, FunctionTree<D> &,
-          std::function<double (std::array<double, D>)>, int>
-          (&vampyr::project<D>),
+          std::function<double (const Coord<D> &)>, int>
+          (&mrcpp::project<D>),
           py::arg("precision"), py::arg("output_tree"), py::arg("function"),
           py::arg("maxIter")= -1,
           "Projects an analytic function onto a FunctionTree");
 
-    m.def("project", py::overload_cast<double, FunctionTree<D> &, RepresentableFunction<D> &, int>(&mrcpp::project<D>),
-          py::arg("precision"), py::arg("output_tree"), py::arg("GaussFunc"), py::arg("maxIter") = -1);
+    m.def("project", py::overload_cast<double, FunctionTree<D> &,
+          RepresentableFunction<D> &, int>(&mrcpp::project<D>),
+          py::arg("precision"), py::arg("output_tree"), py::arg("GaussFunc"),
+          py::arg("maxIter") = -1);
 
-    py::class_<RepresentableFunction<D>, PyRepresentableFunction<D>> repfunc(m, "RepresentableFunction");
+    py::class_<RepresentableFunction<D>, PyRepresentableFunction<D>>
+    repfunc(m, "RepresentableFunction");
     repfunc
         .def(py::init<>());
 
