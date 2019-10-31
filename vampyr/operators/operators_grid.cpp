2710c94e426fff2191045f2eb69f7a1987721505
Roberto Di Remigio <robertodr@users.noreply.github.com>
2019-10-31 11:05:30 +0100

    Fix binding for one refine_grid overload

--- vampyr/operators/operators_grid.cpp
+++ vampyr/operators/operators_grid.cpp
@@ -52,10 +52,7 @@ void operators_grid(py::module &m) {
 
     m.def("clear_grid", py::overload_cast<FunctionTree<D> &>(&clear_grid<D>));
     m.def("refine_grid", py::overload_cast<FunctionTree<D> &, int>(&refine_grid<D>), "out"_a, "scales"_a);
-
-    m.def("refine_grid", py::overload_cast<FunctionTree<D> &, double>(&refine_grid<D>), "out"_a, "prec"_a);
+    m.def("refine_grid", py::overload_cast<FunctionTree<D> &, double, bool>(&refine_grid<D>), "out"_a, "prec"_a, "abs_prec"_a = false);
     m.def("refine_grid", py::overload_cast<FunctionTree<D> &, FunctionTree<D> &>(&refine_grid<D>), "out"_a, "inp"_a);
-
-
 }
 } // namespace vampyr
