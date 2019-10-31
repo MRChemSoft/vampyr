333950643865a8304934d5223a1b5cd1104340ab
Roberto Di Remigio <robertodr@users.noreply.github.com>
2019-10-31 11:18:18 +0100

    Fix compilation of project bindings

--- vampyr/project/project.cpp
+++ vampyr/project/project.cpp
@@ -18,18 +18,20 @@ void project(py::module &m) {
 
     m.def(
         "project",
-        py::overload_cast<double, FunctionTree<D> &, std::function<double(const Coord<D> &)>, int>(&mrcpp::project<D>),
+        py::overload_cast<double, FunctionTree<D> &, std::function<double(const Coord<D> &)>, int, bool>(&mrcpp::project<D>),
         "precision"_a,
         "output_tree"_a,
         "function"_a,
         "maxIter"_a = -1,
+        "abs_prec"_a = false,
         "Projects an analytic function onto a FunctionTree");
 
     m.def("project",
-          py::overload_cast<double, FunctionTree<D> &, RepresentableFunction<D> &, int>(&mrcpp::project<D>),
+          py::overload_cast<double, FunctionTree<D> &, RepresentableFunction<D> &, int, bool>(&mrcpp::project<D>),
           "precision"_a,
           "output_tree"_a,
           "GaussFunc"_a,
-          "maxIter"_a = -1);
+          "maxIter"_a = -1,
+          "abs_prec"_a = false);
 }
 } // namespace vampyr
