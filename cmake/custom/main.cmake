49be20a63296a9e4b0ea614945c5f65fa08ba1d8
Roberto Di Remigio <roberto.diremigio@gmail.com>
2020-10-21 20:16:54 +0200

    Prepare first release
    - Added a workflow for automatic tagging with [Palisade](https://github.com/lightspeed/palisade/blob/master/doc/usage.md)
    - Added `.zenodo.json`
    - `__version__` is properly set according to contents of `VERSION`
    - The version of MRCPP can be retrieved from Python by calling `mrcpp_version()`

--- cmake/custom/main.cmake
+++ cmake/custom/main.cmake
@@ -1,3 +1,11 @@
+# read PROGRAM_VERSION from file
+if(EXISTS "${PROJECT_SOURCE_DIR}/VERSION")
+  file(READ "${PROJECT_SOURCE_DIR}/VERSION" PROGRAM_VERSION)
+  string(STRIP "${PROGRAM_VERSION}" PROGRAM_VERSION)
+else()
+  message(FATAL_ERROR "File ${PROJECT_SOURCE_DIR}/VERSION not found")
+endif()
+
 if(NOT DEFINED PYMOD_INSTALL_LIBDIR)
   message(STATUS "Setting (unspecified) option PYMOD_INSTALL_LIBDIR: python")
   set(PYMOD_INSTALL_LIBDIR "python" CACHE STRING "Location within lib to which Python modules are installed" FORCE)
