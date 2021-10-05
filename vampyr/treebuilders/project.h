42dde58726c2eb1a11cb92521765fa1d207180d0
Stig Rune Jensen <stig.r.jensen@uit.no>
2021-10-05 09:00:26 +0200

    Circumvent GIL in projection of analytic function

--- vampyr/treebuilders/project.h
+++ vampyr/treebuilders/project.h
@@ -23,14 +23,16 @@ template <int D> void project(pybind11::module &m) {
           "max_iter"_a = -1,
           "abs_prec"_a = false);
 
-    m.def("project",
-          py::overload_cast<double,
-                            FunctionTree<D> &,
-                            std::function<double(const Coord<D> &r)>,
-                            int,
-                            bool>
-                            (&mrcpp::project<D>),
-          py::call_guard<py::gil_scoped_release>(),
+    m.def("project", [] (double prec,
+                         FunctionTree<D> &out,
+                         std::function<double(const Coord<D> &r)> inp,
+                         int max_iter,
+                         bool abs_prec) {
+              auto old_threads = mrcpp_get_num_threads();
+              mrcpp::set_max_threads(1);
+              mrcpp::project<D>(prec, out, inp, max_iter, abs_prec);
+              mrcpp::set_max_threads(old_threads);
+          },
           "prec"_a = -1.0,
           "out"_a,
           "inp"_a,
