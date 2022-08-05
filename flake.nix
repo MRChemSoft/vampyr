4db1fe6f69d6024cb52031f79617bb6e9b41b3e6
Roberto Di Remigio <roberto.diremigio@gmail.com>
2022-08-05 11:48:07 +0200

    Add some ancillary files (env-management)

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "VAMPyR";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pypi-deps-db = {
+      url = "github:DavHau/pypi-deps-db";
+      flake = false;
+    };
+    mach-nix = {
+      url = "mach-nix/3.4.0";
+      inputs.pypi-deps-db.follows = "pypi-deps-db";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, pypi-deps-db, mach-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pythonEnv = mach-nix.lib."${system}".mkPython {
+          requirements = builtins.readFile ./requirements.txt + ''
+            # additional dependencies for local work
+            #jupyterlab
+          '';
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            cmake
+            eigen
+            gcc
+            openmpi
+            ninja
+          ];
+          buildInputs = [
+            pythonEnv
+          ];
+          NINJA_STATUS = "[Built edge %f of %t in %e sec] ";
+        };
+      });
+}
