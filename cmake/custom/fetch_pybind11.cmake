415c083ac8128f19acaeeef817ecc7b993b8fa5e
Magnar Bjorgve <magnbjor@gmail.com>
2023-07-16 13:01:15 +0200

    Update pybind so it works with newer python versions (#103)

--- cmake/custom/fetch_pybind11.cmake
+++ cmake/custom/fetch_pybind11.cmake
@@ -1,4 +1,4 @@
-find_package(pybind11 2.6 CONFIG QUIET)
+find_package(pybind11 2.9 CONFIG QUIET)
 
 set(PYBIND11_CPP_STANDARD "-std=c++${CMAKE_CXX_STANDARD}")
 
@@ -10,9 +10,8 @@ else()
   FetchContent_Declare(pybind11
     QUIET
     URL
-      https://github.com/pybind/pybind11/archive/v2.7.1.tar.gz
+      https://github.com/pybind/pybind11/archive/v2.10.0.tar.gz
     )
   set(PYBIND11_TEST OFF CACHE BOOL "")
   FetchContent_MakeAvailable(pybind11)
 endif()
-
