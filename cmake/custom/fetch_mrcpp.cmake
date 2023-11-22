42da85d180ba1d0ac5ad56573cd3137efe983d96
edinvay <evgeni36@yandex.ru>
2023-11-22 16:01:44 +0100

    linked vampyr back to the master mrcpp branch

--- cmake/custom/fetch_mrcpp.cmake
+++ cmake/custom/fetch_mrcpp.cmake
@@ -16,9 +16,9 @@ else()
   FetchContent_Declare(mrcpp
     QUIET
     GIT_REPOSITORY
-      https://github.com/edinvay/mrcpp.git
+      https://github.com/MRChemSoft/mrcpp.git
     GIT_TAG
-      timeevol
+      master
     )
 
   set(CMAKE_CXX_COMPILER ${CMAKE_CXX_COMPILER})
