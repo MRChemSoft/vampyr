f0b4f6599607d391c202b3a37bcc0a257fc6438d
Valentyn113 <119410236+Valentyn113@users.noreply.github.com>
2026-01-28 15:42:06 +0100

    Update test_basis.py
    trapz

--- src/vampyr/tests/test_basis.py
+++ src/vampyr/tests/test_basis.py
@@ -1,8 +1,9 @@
-import numpy as np
+dimport numpy as np
 import pytest
 
 import vampyr as vp
 
+trapezoid = getattr(np, "trapezoid", np.trapz)
 
 def test_LegendreBasis():
     basis = vp.LegendreBasis(order=5)
@@ -30,7 +31,7 @@ def test_InterpolatingScalingBasis():
     dx = 0.0001
     x = np.arange(0, 0.51, dx)
     f_squared = [f([x]) * f([x]) for x in x]
-    assert 1.0 == pytest.approx(np.trapz(f_squared, dx=dx), 0.001)
+    assert 1.0 == pytest.approx(np.trapezoid(f_squared, dx=dx), 0.001)
 
 
 def test_InterpolatingWaveletBasis():
@@ -39,7 +40,7 @@ def test_InterpolatingWaveletBasis():
     dx = 0.0001
     x = np.arange(0, 0.51, dx)
     f_squared = [f([x]) * f([x]) for x in x]
-    assert 1.0 == pytest.approx(np.trapz(f_squared, dx=dx), 0.001)
+    assert 1.0 == pytest.approx(np.trapezoid(f_squared, dx=dx), 0.001)
 
 
 def test_LegendreScalingBasis():
@@ -48,7 +49,7 @@ def test_LegendreScalingBasis():
     dx = 0.0001
     x = np.arange(0, 0.51, dx)
     f_squared = [f([x]) * f([x]) for x in x]
-    assert 1.0 == pytest.approx(np.trapz(f_squared, dx=dx), 0.001)
+    assert 1.0 == pytest.approx(np.trapezoid(f_squared, dx=dx), 0.001)
 
 
 def test_LegendreWaveletBasis():
